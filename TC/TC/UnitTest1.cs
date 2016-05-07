6e0c5dd6c79f7dc3fe79cf2eb3424142a842e372
xins Bin <q25a25q@live.com>
2016-05-07 17:30:51 +0800

    1

---
+++ TC/TC/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TC
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
